05e3588627754bbf1e5f2880a250bc63bf492f56
luanshiqiguo <1484836966@qq.com>
2020-07-05 15:08:24 +0800

    登录bug
    添加是否登录

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,28 +7,57 @@ const { weatherList } = require('../../data/json.js');
 Page({
   data: {
     userInfo: {},
-    hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     weatherList: jsonData.weatherList,
     searchCity: '',
     pics:[],
     isShow: true,
-    fromLogin: true
+    isHide: false
   },
   
   onLoad: function () {
+    var that = this;
+    this.getWeather("北京")
+    wx.getSetting({
+      success: function(res) {
+          if (res.authSetting['scope.userInfo']) {
+              wx.getUserInfo({
+                  success: function(res) {
+                      // 用户已经授权过,不需要显示授权页面,所以不需要改变 isHide 的值
+                      // 根据自己的需求有其他操作再补充
+                      // 我这里实现的是在用户授权成功后，调用微信的 wx.login 接口，从而获取code
+                      that.setData({
+                        userInfo: res.userInfo
+                      });
+                      if (this.userInfoReadyCallback) {
+                        this.userInfoReadyCallback(res)
+                      }
+                      wx.login({
+                          success: res => {
+                              // 获取到用户的 code 之后：res.code
+                          }
+                      });
+                  }
+              });
+          } else {
+              // 用户没有授权
+              // 改变 isHide 的值，显示授权页面
+              that.setData({
+                  isHide: true
+              });
+          }
+      }
+    });
     if (app.globalData.userInfo) {
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        userInfo: app.globalData.userInfo
       })
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          userInfo: res.userInfo
         })
       }
     } else {
@@ -37,22 +66,12 @@ Page({
         success: res => {
           app.globalData.userInfo = res.userInfo
           this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+            userInfo: res.userInfo
           })
         }
       })
     }
-    this.getWeather("北京")
     this.animatFn()
-  },
-  getUserInfo: function(e) {
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo || {},
-      hasUserInfo: e.detail.userInfo ? true : false,
-      fromLogin: true
-    })
   },
   getWeather: function(e) {
     let that = this
@@ -68,7 +87,6 @@ Page({
           })
           return;
         }
-        //console.log(res)
         that.setData({
           city: e,
           // 温度
@@ -142,13 +160,10 @@ Page({
   clickSearch: function(){
     this.getWeather(this.data.searchCity)
   },
-  //事件处理函数
+  //点击头像自定义背景
   bindViewTap: function() {
-    // wx.navigateTo({
-    //   url: '../logs/logs'
-    // })
     this.setData({
-      fromLogin: false
+      isHide: true
     })
     console.log('跳转上传页')
   },
@@ -176,8 +191,7 @@ Page({
         }
         that.setData({
           isShow: false,
-          pics: pics,
-          fromLogin: true
+          pics: pics
         })
       },
     })
@@ -217,8 +231,34 @@ Page({
         next = !next
       }
       this.setData({
-        animat: animat.export()
+        animat: animat
       })
     }, 7000)
+  },
+  bindGetUserInfo: function(e) {
+    if (e.detail.userInfo) {
+        //用户按了允许授权按钮
+        var that = this;
+        //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
+        that.setData({
+            isHide: false,
+            userInfo: e.detail.userInfo,
+            haveClick: true
+        });
+    } else {
+        //用户按了拒绝按钮
+        wx.showModal({
+            title: '警告',
+            content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入哦',
+            showCancel: false,
+            confirmText: '返回授权',
+            success: function(res) {
+                // 用户没有授权成功，不需要改变 isHide 的值
+                if (res.confirm) {
+                    console.log('用户点击了“返回授权”');
+                }
+            }
+        });
+    }
   }
 })
